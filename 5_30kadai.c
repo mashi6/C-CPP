285488047734ad794ad6d32e8d29b18114991b27
mashi6 <e1626@s.akashi.ac.jp>
2017-05-30 19:01:07 +0900

    5/30

---
+++ 5_30kadai.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+double Pythagorean(int a,int b){
+	return sqrt(pow(a,2)+pow(b,2));
+}
+
+int main(void){
+	srandom( (unsigned)time(NULL) );
+	double pi;
+	double totalPi=0;
+	int inCircle=0,outCircle=0;
+	long long int x,y;
+	for(int t = 0;t<1000;t++){
+		inCircle = 0;
+		outCircle = 0;
+
+	for (int i=0;i<=100000;i++){
+		x = (random()*random())%100000000;
+
+		y = (random()*random())%100000000;
+
+		if (Pythagorean(x,y)<100000000.0){
+			inCircle++;
+
+		}else{
+			outCircle++;
+
+		}
+}
+
+	// printf("in : out = %d : %d\n",inCircle,outCircle);
+	pi = (double)(4.0*(double)(inCircle))/(double)(inCircle+outCircle);
+	totalPi+=pi;
+	// printf("pi : %f\n",pi);
+}
+	printf("Average pi : %f\n",totalPi/1000.0);
+
+	return 0;
+}
---
+++ test3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void){
+	while(1){
+		printf("%ld\n",random());
+	}
+	return 0;
+}
